19975944e73724dca759596efab2215f9772bc23
Ariv Debnath <arivdebnath10@gmail.com>
2022-01-02 18:27:33 +0530

    updated saving functionality

--- utils/savePassword.js
+++ utils/savePassword.js
@@ -7,7 +7,7 @@ const savePassword = (password) => {
     fs.open(path.join(__dirname, '../', 'passwords.txt'), 'a', 766, (e, id) => {
         fs.write(id, password + os.EOL, null, 'utf-8', () => {
             fs.close(id, () => {
-                console.log(chalk.blue.bold('Password copied to passwords.txt'));
+                console.log(chalk.blue.bold('Password saved to passwords.txt'));
             })
         })
     })
